52181951db40cd211361b9682d38a7743b401bdb
arkabrian <arka.brian13@gmail.com>
2022-07-21 00:45:17 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', './assets/js/*.js', './node_modules/tw-elements/dist/js/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
