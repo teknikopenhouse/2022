49052133beefa90b4d8d1bb9cab06f47e3ac5ba6
arkabrian <arka.brian13@gmail.com>
2022-08-20 23:38:59 +0700

    footer jadi

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ var swiper = new Swiper('.blog-slider', {
   mousewheel: {
     invert: false,
   },
-  // autoHeight: true,
   pagination: {
     el: '.blog-slider__pagination',
     clickable: true,
